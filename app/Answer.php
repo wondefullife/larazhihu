d00cd70f0a7164a14f5c18f73aff0a53938e66ad
Append <appendtrue@gmail.com>
2020-07-28 21:06:33 +0800

    新增最佳答案功能 仅问题创建者可以标记为最佳答案

--- app/Answer.php
+++ app/Answer.php
@@ -9,4 +9,14 @@ class Answer extends Model
     protected $fillable = [
         'user_id', 'content', 'question_id'
     ];
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    public function isBest()
+    {
+        return $this->id == $this->question->best_answer_id;
+    }
 }
