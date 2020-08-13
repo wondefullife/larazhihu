bbe774c19824c75ecf45c149ce14715d6c6ffbcf
Append <appendtrue@gmail.com>
2020-08-13 09:59:38 +0800

    重构 vote 模型代码

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\VoteTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    use VoteTrait;
+
     protected $fillable = [
         'user_id', 'content', 'question_id'
     ];
@@ -20,63 +23,8 @@ class Answer extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function votes($type)
-    {
-        return $this->morphMany(Vote::class, 'voted')->whereType($type);
-    }
-
     public function isBest()
     {
         return $this->id == $this->question->best_answer_id;
-    }
-
-    public function voteUp($user)
-    {
-        if ($this->votes('vote_up')->where(['user_id' => $user->id])->doesntExist()) {
-            $this->votes('vote_up')->create(['user_id' => $user->id, 'type' => 'vote_up']);
-        }
-    }
-
-    public function voteDown($user)
-    {
-        if ($this->votes('vote_down')->where(['user_id' => $user->id])->doesntExist()) {
-            $this->votes('vote_down')->create(['user_id' => $user->id, 'type' => 'vote_down']);
-        }
-    }
-
-    public function cancelVoteUp($user)
-    {
-        $this->votes('vote_up')->where(['user_id' => $user->id])->delete();
-    }
-
-    public function cancelVoteDown($user)
-    {
-        $this->votes('vote_down')->where('user_id', $user->id)->delete();
-    }
-
-    public function isVotedUp(User $user)
-    {
-        if (!$user) {
-            return false;
-        }
-        return $this->votes('vote_up')->where('user_id', $user->id)->exists();
-    }
-
-    public function isVotedDown(User $user)
-    {
-        if (!$user) {
-            return false;
-        }
-        return $this->votes('vote_down')->where('user_id', $user->id)->exists();
-    }
-
-    public function getUpVotesCountAttribute()
-    {
-        return $this->votes('vote_up')->count();
-    }
-
-    public function getDownVotesCountAttribute()
-    {
-        return $this->votes('vote_down')->count();
     }
 }
---
+++ app/Models/Traits/VoteTrait.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models\Traits;
+
+use App\Models\User;
+use App\Models\Vote;
+
+trait VoteTrait
+{
+    public function votes($type)
+    {
+        return $this->morphMany(Vote::class, 'voted')->whereType($type);
+    }
+
+    protected function vote($user, $type)
+    {
+        if ($this->votes($type)->where(['user_id' => $user->id])->doesntExist()) {
+            $this->votes($type)->create(['user_id' => $user->id, 'type' => $type]);
+        }
+    }
+
+    public function voteUp($user)
+    {
+        $this->vote($user, 'vote_up');
+    }
+
+    public function voteDown($user)
+    {
+        $this->vote($user, 'vote_down');
+    }
+
+    protected function cancelVote($user, $type)
+    {
+        $this->votes($type)->where(['user_id' => $user->id])->delete();
+    }
+
+    public function cancelVoteUp($user)
+    {
+        $this->cancelVote($user, 'vote_up');
+    }
+
+    public function cancelVoteDown($user)
+    {
+        $this->cancelVote($user, 'vote_down');
+    }
+
+    protected function isVoted(User $user, $type)
+    {
+        if (!$user) {
+            return false;
+        }
+        return $this->votes($type)->where('user_id', $user->id)->exists();
+    }
+
+    public function isVotedUp(User $user)
+    {
+        return $this->isVoted($user, 'vote_up');
+    }
+
+    public function isVotedDown(User $user)
+    {
+        return $this->isVoted($user, 'vote_down');
+    }
+
+    public function getUpVotesCountAttribute()
+    {
+        return $this->votes('vote_up')->count();
+    }
+
+    public function getDownVotesCountAttribute()
+    {
+        return $this->votes('vote_down')->count();
+    }
+}
